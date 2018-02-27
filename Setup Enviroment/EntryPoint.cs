dacd91946c4fc60fd97b2d2491100a06a0b00e42
Deboh <david.ebohttu@gmail.com>
2018-02-26 23:22:05 -0600

    .01 Name Selection Practice

---
+++ Setup Enviroment/EntryPoint.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://testing.todvachev.com/selectors/name/");
+
+        IWebElement element = driver.FindElement(By.Name("myName"));
+
+        if (element.Displayed)
+        {
+            System.Console.WriteLine("Yes! I can see the element, its right there!");
+        }
+        else
+        {
+            System.Console.WriteLine("Well, something went wrong, I couldn't see the element");
+        }
+
+        driver.Quit();
+    }
+}
