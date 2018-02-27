4b2ed9b212b931b1a33d74eb434f91dc878d5f7f
Deboh <david.ebohttu@gmail.com>
2018-02-27 01:02:49 -0600

    Here we will have a simple element that you will be able to select through a Class Name. But be careful when you use Class Names, they are usually used to target more than one element, so the result may not always be what you would expect it to be.
    the printed result on the console is: This is a paragraph with text that belongs to a class.

---
+++ ConsoleApp2/EntryPoint.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/selectors/class-name/";
+        string className = "testClass";
+
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://testing.todvachev.com/selectors/class-name/");
+
+        IWebElement element = driver.FindElement(By.ClassName(className));
+
+        Console.WriteLine(element.Text);
+
+        driver.Quit();
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
